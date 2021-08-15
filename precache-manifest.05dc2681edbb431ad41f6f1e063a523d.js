d53bf0b4fc205cb7e24b81e6d63bac9933bc877b
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-15 22:29:56 +0300

    Updates

---
+++ precache-manifest.05dc2681edbb431ad41f6f1e063a523d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbc0cd79e4bc4347527c779106e6063",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e29d8ea312efa16a7c5",
+    "url": "./static/css/main.e1393bab.chunk.css"
+  },
+  {
+    "revision": "9f0eee71a07e9084ee66",
+    "url": "./static/js/2.991c3b20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.991c3b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e29d8ea312efa16a7c5",
+    "url": "./static/js/main.a290fb6d.chunk.js"
+  },
+  {
+    "revision": "84c8f6a96466fa846724",
+    "url": "./static/js/runtime-main.dc4a9e8a.js"
+  },
+  {
+    "revision": "cd6cbbfa2ecd589a7a7975c99d428433",
+    "url": "./static/media/bck_casino.cd6cbbfa.jpg"
+  },
+  {
+    "revision": "4515f4755d43991cbefbc03e41055504",
+    "url": "./static/media/logo.4515f475.png"
+  }
+]);
